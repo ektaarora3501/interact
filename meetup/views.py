32001a50c59c523c52f2ff0ea9f57eab10175bd3
ektaarora3501 <ektaarora3501@gmail.com>
2019-09-15 22:06:34 +0530

    Changing redirect routes

--- meetup/views.py
+++ meetup/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
-from meetup.models import RegisMentor
-from meetup.forms import RegisMentorForm
+from meetup.models import Register_user
+from meetup.forms import RegisterForm
 # Create your views here.
 from django.http import HttpResponseRedirect
 from django.urls import reverse,reverse_lazy
@@ -13,39 +13,39 @@ import datetime
 def index(request):
     return render(request,'home.html')
 
-def RgMentor(request):
+def Register(request):
     if request.method=='POST':
-       form =RegisMentorForm(request.POST)
+       form =RegisterForm(request.POST)
        print("post")
 
        if form.is_valid():
            user=form.cleaned_data['first_name']
            print(user)
-           us=RegisMentor()
+           us=Register_user()
            #url=confirm/user
            print("form valid")
            us.first_name=form.cleaned_data['first_name']
            us.last_name=form.cleaned_data['last_name']
            us.email=form.cleaned_data['email']
-           us.field=form.cleaned_data['choice']
-           us.grad_year=form.cleaned_data['grad_year']
-           us.Batch_id=form.cleaned_data['Batch_id']
+           us.branch=form.cleaned_data['choice']
+           us.curr_year=form.cleaned_data['curr_year']
+           us.roll_no=form.cleaned_data['adm_no']
            us.save()
-           print("field_value=",us.field)
+           print("branch,curr_year",us.branch,us.curr_year)
 
            return HttpResponseRedirect(reverse('confirm_regis',args=(user,)))
 
     else:
         #proposed_date=datetime.date.today()+datetime.timedelta(weeks=3)
 
-        form=RegisMentorForm()
+        form=RegisterForm()
 
     context={
     'form':form,
     }
 
 
-    return render(request,'MentorRegis.html',context)
+    return render(request,'Register.html',context)
 
 def confirm(request,user):
     context={
