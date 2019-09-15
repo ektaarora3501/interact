7eb6bb9cd0a9b746eba4d36a1b0e9e738e468aa1
ektaarora3501 <ektaarora3501@gmail.com>
2019-09-15 22:06:13 +0530

    Database models

--- meetup/models.py
+++ meetup/models.py
@@ -3,14 +3,14 @@ from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 import datetime
 
-class RegisMentor(models.Model):
+class Register_user(models.Model):
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
-    field=models.CharField(max_length=100)
+    branch=models.CharField(max_length=100)
     email=models.EmailField(unique=True)
-    date=models.DateField(_("Date"),default=datetime.date.today)
-    grad_year=models.CharField("Graduation year",max_length=4,default=None)
-    Batch_id=models.CharField("Batch Id",max_length=8,default=None)
+    #date=models.DateField(_("Date"),default=datetime.date.today)
+    curr_year=models.CharField(max_length=100)
+    roll_no=models.CharField(max_length=8,default=None)
 
     def __str__(self):
         return f'{self.first_name},{self.email},{self.field}'
