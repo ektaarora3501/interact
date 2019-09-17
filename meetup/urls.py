1bb6927eff9bce93774e9a403cd1ec33a43d7b61
ektaarora3501 <ektaarora3501@gmail.com>
2019-09-17 12:21:49 +0530

    Adding routes for login and dashboard urls

--- meetup/urls.py
+++ meetup/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('',views.index,name="home"),
     path('Register',views.Register,name="register"),
     path('confirm/<user>',views.confirm,name='confirm_regis'),
+    path('login',views.login,name='login_user'),
+    path('user_dashboard/<user>',views.dashboard,name='user-dashboard'),
 ]
