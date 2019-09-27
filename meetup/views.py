18853f393fa87a5e8b309facb551418406534c90
ektaarora3501 <ektaarora3501@gmail.com>
2019-09-27 11:29:42 +0530

    Adding user sessions

--- meetup/views.py
+++ meetup/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,redirect
 from meetup.models import Register_user
 from meetup.forms import RegisterForm,LoginForm
-# Create your views here.
 from django.http import HttpResponseRedirect
 from django.urls import reverse,reverse_lazy
 from django.shortcuts import redirect
@@ -9,6 +8,7 @@ from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 import datetime
+import bcrypt
 
 def index(request):
     return render(request,'home.html')
@@ -25,16 +25,20 @@ def Register(request):
            #url=confirm/user
            print("form valid")
            us.first_name=form.cleaned_data['first_name']
+           name=form.cleaned_data['first_name']
            us.last_name=form.cleaned_data['last_name']
            us.email=form.cleaned_data['email']
            us.branch=form.cleaned_data['choice']
            us.curr_year=form.cleaned_data['curr_year']
            us.roll_no=form.cleaned_data['adm_no']
-           us.password=form.cleaned_data['password']
+           user=form.cleaned_data['password']
+           user=user.encode('utf-8')
+           us.password=bcrypt.hashpw(user,bcrypt.gensalt())
+           print(us.password)
            us.save()
            print("branch,curr_year",us.branch,us.curr_year)
 
-           return HttpResponseRedirect(reverse('confirm_regis',args=(user,)))
+           return HttpResponseRedirect(reverse('confirm_regis',args=(name,)))
 
     else:
         #proposed_date=datetime.date.today()+datetime.timedelta(weeks=3)
