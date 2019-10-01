6cdd4a97a17982eefb9136b8ed3b8822cbf84e3e
ektaarora3501 <ektaarora3501@gmail.com>
2019-10-02 01:07:07 +0530

    Adding changes for image upload

--- asmp/urls.py
+++ asmp/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,6 @@ urlpatterns+=[
 #urlpatterns+=[
  #  path('',RedirectView.as_view(url='/asmp/',permanent=True))
 #]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
