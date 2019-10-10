70667c9d45b5dc1d4a02f21d7db5aea36f341248
ektaarora3501 <ektaarora3501@gmail.com>
2019-10-10 19:30:15 +0530

    Settings for sending mails

--- asmp/settings.py
+++ asmp/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '************@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxx'
+EMAIL_PORT = 587
 
 # Application definition
 
