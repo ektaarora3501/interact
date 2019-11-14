ff072883af00e0d1759632985563089a8fe4f475
Ekta <ektaarora3501@gmail.com>
2019-11-14 13:23:32 +0530

    Adding Admin urls

--- meetup/urls.py
+++ meetup/urls.py
@@ -12,4 +12,7 @@ urlpatterns=[
     path('filter/member/<group>',views.get_member,name='member_filter'),
     path('study_stuff',views.material,name="studyy_stuff"),
     path('send_mail',views.contact,name="contact-us"),
+    path('admin/set_notice',views.set_notice,name="notice"),
+    path('admin/show_event',views.show_event,name='show_event'),
+    path('admin/delete_event/<id>',views.delete_event,name='delete_event')
 ]
